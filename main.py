3d97d909e2cfab56a89ef1fc1f73d3bb3e73eda7
anushasonthalia <anusha.sonthalia@columbia.edu>
2025-11-23 22:01:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from typing import Dict, List, Optional
 from uuid import UUID, uuid4
 
 from fastapi import FastAPI, HTTPException, Path, status
-from sqlalchem.orm import Session
+from sqlalchemy.orm import Session
 
 from models.preferences import PreferenceCreate, PreferenceRead, PreferenceUpdate
 from database import Base, engine, get_db
