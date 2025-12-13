81f40752a82dd12f54e7a191701c3f080a378ed1
anushasonthalia <anusha.sonthalia@columbia.edu>
2025-12-13 12:45:19 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,7 +19,7 @@ DATABASE_URL = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_nam
 # create the SQLAlchemy engine and session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+Base = DeclarativeBase()
 
 def get_db():
     db = SessionLocal()
